c9d189229e4842060439431449ae8bb2231f6b08
Pablo Diaz R <pablo.diaz.r85@gmail.com>
2023-07-08 16:16:21 -0400

    Modificado formato de código

--- chat-app-backend/controllers/user-controller.js
+++ chat-app-backend/controllers/user-controller.js
@@ -4,23 +4,20 @@ const ApiError = require('../errors/api-error');
 
 const registerUser = asyncHandler(async (req, res) => {
   const { displayName, email, password, confirmPassword } = req.body;
+  
   if (!displayName || !email || !password || !confirmPassword) {
     throw new ApiError(400, 'Todos los campos son requeridos');
   }
-
   const userExists = await User.findOne({ email });
   if (userExists) {
     throw new ApiError(409, 'Ya existe un usuario registrado con este email');
   }
-
   if (password.length < 6 || password.length > 20) {
     throw new ApiError(400, 'El password debe tener entre 6 y 20 caracteres');
   }
-
   if (password !== confirmPassword) {
     throw new ApiError(400, 'Los passwords no concuerdan');
   }
-
   const createdUser = await User.create({
     displayName,
     email,
