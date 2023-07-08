37774f98b5883a6f1adf80e894a2dc1140395296
Pablo Diaz R <pablo.diaz.r85@gmail.com>
2023-07-08 17:25:28 -0400

    Modificado, Se añade campo refreshToken, se previene envío de password y refreshToken

--- chat-app-backend/models/user-model.js
+++ chat-app-backend/models/user-model.js
@@ -2,39 +2,51 @@ const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
 const bcrypt = require('bcryptjs');
 
-const UserSchema = mongoose.Schema({
-  displayName: {
-    type: String,
-    required: [true, 'Debe ingresar un nombre'],
-    minlength: [3, 'Nombre demasiado corto: mínimo 3 caracteres'],
-    maxlength: [20, 'Nombre demasiado largo. máximo 20 caracteres'],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    validate: {
-      validator: isEmail,
-      message: 'El email ingresado no es válido',
+const UserSchema = mongoose.Schema(
+  {
+    displayName: {
+      type: String,
+      required: [true, 'Debe ingresar un nombre'],
+      minlength: [3, 'Nombre demasiado corto: mínimo 3 caracteres'],
+      maxlength: [20, 'Nombre demasiado largo. máximo 20 caracteres'],
+      trim: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: isEmail,
+        message: 'El email ingresado no es válido',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    status: {
+      type: String,
+      enum: ['1', '2', '3'],
+      default: '1',
+    },
+    refreshToken: { type: String },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  avatar: {
-    type: String,
-    default: '',
-  },
-  status: {
-    type: String,
-    enum: ['1', '2', '3'],
-    default: '1',
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.refreshToken;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 UserSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
