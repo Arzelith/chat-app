4e2179c87d18c2bc01f9a2e62b7f8ce336e579f1
Pablo Diaz R <pablo.diaz.r85@gmail.com>
2023-07-15 19:33:42 -0400

    Modificado, se incorporan AlertDisplay y PaperWrapper

--- chat-app-frontend/src/components/index.js
+++ chat-app-frontend/src/components/index.js
@@ -1,5 +1,7 @@
 import TextInput from './TextInput';
 import PageWrapper from './PageWrapper';
 import ButtonInput from './ButtonInput';
+import AlertDisplay from './AlertDisplay';
+import PaperWrapper from './PaperWrapper';
 
-export { TextInput, PageWrapper, ButtonInput };
+export { TextInput, PageWrapper, ButtonInput, AlertDisplay, PaperWrapper };
